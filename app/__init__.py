68bc741b18eb15cc43e9cdb06d0756ac90d34516
RoboMo <65652833+RobertMochnacky@users.noreply.github.com>
2025-10-15 14:47:15 +0200

    Create __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # load .env
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.login_view = "auth.login"
+migrate = Migrate()
+
+def create_app():
+    app = Flask(__name__)
+    app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "devkey")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
+    login_manager.init_app(app)
+    migrate.init_app(app, db)
+
+    # Blueprints
+    from .auth import auth_bp
+    app.register_blueprint(auth_bp, url_prefix="/auth")
+
+    from .main import main_bp
+    app.register_blueprint(main_bp)
+
+    return app
