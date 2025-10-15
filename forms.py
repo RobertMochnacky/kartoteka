0cc7a013ed9a37d0526fd198fd5a76f6fe7d11dd
RoboMo <65652833+RobertMochnacky@users.noreply.github.com>
2025-10-15 11:46:07 +0200

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,27 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, TextAreaField, SubmitField, DateField
-from wtforms.validators import DataRequired, Email, EqualTo, Length
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+from email_validator import validate_email, EmailNotValidError
 
 class LoginForm(FlaskForm):
-    username = StringField("Username", validators=[DataRequired()])
-    password = PasswordField("Password", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=6)])
     submit = SubmitField("Login")
 
 class RegisterForm(FlaskForm):
-    username = StringField("Username", validators=[DataRequired(), Length(min=3, max=64)])
     email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired(), Length(min=6)])
-    confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo("password")])
     submit = SubmitField("Register")
-
-class ClientForm(FlaskForm):
-    name = StringField("Client name", validators=[DataRequired()])
-    contact = StringField("Contact info")
-    notes = TextAreaField("Notes")
-    submit = SubmitField("Save Client")
-
-class ActivityForm(FlaskForm):
-    activity_date = DateField("Activity date", validators=[DataRequired()], format="%Y-%m-%d")
-    person_name = StringField("Person responsible")
-    description = TextAreaField("Task description", validators=[DataRequired()])
-    submit = SubmitField("Add Activity")
