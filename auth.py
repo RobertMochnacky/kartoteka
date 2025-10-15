603b0d128feed7afa50c6dcb7e370ee15efdbea5
RoboMo <65652833+RobertMochnacky@users.noreply.github.com>
2025-10-15 13:45:28 +0200

    Update auth.py

--- auth.py
+++ auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, redirect, url_for, flash, request
 from flask_login import login_user, logout_user, login_required, current_user
 from models import db, User
 from forms import RegisterForm, LoginForm
+from app import login_manager
 
 auth_bp = Blueprint("auth", __name__)
 
@@ -36,3 +37,7 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for("auth.login"))
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
