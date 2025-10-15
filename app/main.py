174547caf74771b93985a24193934bb507df2dd8
RoboMo <65652833+RobertMochnacky@users.noreply.github.com>
2025-10-15 15:38:27 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -7,8 +7,12 @@ from .models import User, Customer
 main_bp = Blueprint("main", __name__)
 
 @main_bp.route("/dashboard")
-#@login_required
 def dashboard():
+    return "<h1>Dashboard works!</h1>"
+
+#@main_bp.route("/dashboard")
+#@login_required
+#def dashboard():
     # Fetch all customers with their activity reports
-    customers = Customer.query.all()
-    return render_template("dashboard.html", user=current_user, customers=customers)
+#    customers = Customer.query.all()
+#    return render_template("dashboard.html", user=current_user, customers=customers)
