552dd460efc2295312815420c0be61321ced4fda
RoboMo <65652833+RobertMochnacky@users.noreply.github.com>
2025-10-15 16:08:36 +0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -20,6 +20,7 @@ class Customer(db.Model):
     __tablename__ = "customers"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(150), nullable=False, unique=True)
     activities = db.relationship("ActivityReport", backref="customer", lazy=True)
 
 
