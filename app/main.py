ac528694f75e137ae286073f4cae357f966749a0
RoboMo <65652833+RobertMochnacky@users.noreply.github.com>
2025-10-15 15:05:44 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
-from models import User, Customer
+from .models import User, Customer
 
 main_bp = Blueprint("main", __name__)
 
