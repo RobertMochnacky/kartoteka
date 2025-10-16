755842ee10992339c5f5131f7f6f5f660bfce893
RoboMo <65652833+RobertMochnacky@users.noreply.github.com>
2025-10-16 13:40:02 +0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -9,6 +9,9 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(256), nullable=False)
+    primary_color = db.Column(db.String(7), default="#3a86ff")  # HEX
+    sidebar_bg_color = db.Column(db.String(7), default="#fff")
+    text_color = db.Column(db.String(7), default="#212529")
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
