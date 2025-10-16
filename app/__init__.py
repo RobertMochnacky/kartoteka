d53f1018fe906c228ac226b415a95ddd914962c9
RoboMo <65652833+RobertMochnacky@users.noreply.github.com>
2025-10-16 16:08:33 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_migrate import Migrate, upgrade
 from dotenv import load_dotenv
 import os
 from sqlalchemy import inspect
+from flask_babel import Babel
 
 load_dotenv()  # load .env
 
@@ -12,16 +13,20 @@ db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.login_view = "auth.login"
 migrate = Migrate()
+babel = Babel()
 
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "devkey")
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+    app.config['BABEL_SUPPORTED_LOCALES'] = ['en', 'sk']  # English and Slovak
 
     db.init_app(app)
     login_manager.init_app(app)
     migrate.init_app(app, db)
+    babel.init_app(app)
 
     # Import models here to avoid circular imports
     from .models import User, Customer, Activity
